3435f3d88f5e06a45f9894721b4ae7f4a0d6346e
PythonHacker24 <adityapatil24680@gmail.com>
2024-02-08 20:51:36 +0530

    Latest Backup

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -14,7 +14,7 @@ static const char col_4[]       = "#7ED957";
 static const char col_5[]        = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_3, col_1, col_2 },
+	[SchemeNorm] = { col_3, col_1, col_1 },
 	[SchemeSel]  = { col_4, col_5,  col_5  },
 };
 
